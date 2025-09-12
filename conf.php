63d814f6e36a832fb8eb5a8c35bfba61da945809
Adan Siba <adan.konse@strathmore.edu>
2025-09-12 22:20:38 +0300

    new project

---
+++ conf.php
@@ -0,0 +1,23 @@
+<?php
+// Site Information
+$conf['site_name'] = 'ICS C Community';
+$conf['site_url'] = 'http://localhost';
+$conf['admin_email'] = 'admin@icsccommunity.com';
+
+// Database Configuration
+$conf['db_type'] = 'pdo';
+$conf['db_host'] = 'localhost';
+$conf['db_user'] = 'root';
+$conf['db_pass'] = '';
+$conf['db_name'] = 'dlone';
+
+// Site Language
+$conf['site_lang'] = 'en';
+
+//Email Configuration
+$conf['mail_type']  = 'smtp';
+$conf['smtp_host_']  = 'smtp.gmail.com';
+$conf['smtp_user']  = 'adan.konse@strathmore.edu';
+$conf['stmp_pass']  = 'dryn jelu bram eczd';
+$conf['stmp_port']  = '465';
+$conf['stmp_secure']  = 'ssl';
